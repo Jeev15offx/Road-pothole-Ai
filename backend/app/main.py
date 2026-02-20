e310dc657350abd7528088343d755ae8b395f936
Jeev15offx <officialjeevan15@gmail.com>
2026-02-20 15:54:49 +0530

    Added init.py and linked precdiction with main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from app.routes.prediction import router as prediction_router
 
 app = FastAPI(title="Road Pothole AI API")
 
+app.include_router(prediction_router)
 @app.get("/")
 def root():
     return {"status": "Backend is running"}
