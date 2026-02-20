04c6e6963cbd147ca822a081a63441c328aed83d
ZygraXe <p.maruthandeepak@gmail.com>
2026-02-20 15:49:43 +0530

    Updated samp_call

--- backend/test/samp_call.py
+++ backend/test/samp_call.py
@@ -1,5 +1,10 @@
 from inference_sdk import InferenceHTTPClient
+import base64
+import os
 
+image_path = "road.jpg"
+print("Exists:", os.path.exists(image_path))
+print("Absolute path:", os.path.abspath(image_path))
 client = InferenceHTTPClient(
     api_url="https://serverless.roboflow.com",
     api_key="S9zy6oG5Kp1FtzScLNKk"
@@ -8,8 +13,15 @@ client = InferenceHTTPClient(
 result = client.run_workflow(
     workspace_name="babu-4meph",
     workflow_id="find-potholes",
-    images={"image": "Road-pothole-Ai/backend/test/img-79.jpg"},
-    use_cache=True
+    images={"image": "road.jpg"}   # <-- STRING PATH
 )
 
+# Extract visualization image
+''' base64_image = result["outputs"][0]["visualization"]["value"]
+
+# Decode and save
+with open("annotated.jpg", "wb") as f:
+    f.write(base64.b64decode(base64_image))
+
+print("Annotated image saved as annotated.jpg") '''
 print(result)
